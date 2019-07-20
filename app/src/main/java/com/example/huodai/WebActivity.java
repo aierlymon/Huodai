c41af905a3df69a0f83bed425e5766aa396018d1
huanghao <1351637684@qq.com>
2019-07-20 22:53:51 +0800

    7.0以下机型好像不适配

---
+++ app/src/main/java/com/example/huodai/WebActivity.java
@@ -0,0 +1,54 @@
+package com.example.huodai;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.baselib.utils.MyLog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+/**
+ * createBy ${huanghao}
+ * on 2019/7/20
+ */
+public class WebActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+        MyLog.i("web url: "+url);
+        WebView webView = ((WebView) findViewById(R.id.webview));
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        String cacheDirPath = getFilesDir().getAbsolutePath()+"/webcache";
+        //设置数据库缓存路径
+        webSettings.setDatabasePath(cacheDirPath);
+        //设置  Application Caches 缓存目录
+        webSettings.setAppCachePath(cacheDirPath);
+        //开启 Application Caches 功能
+        webSettings.setAppCacheEnabled(true);
+
+        webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient());
+
+
+        ((FloatingActionButton) findViewById(R.id.img_back)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
